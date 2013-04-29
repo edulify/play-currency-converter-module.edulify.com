aa9aa6f45698743526a0a0d614c8c8522cf8426e
Ranieri Valença <ranierivalenca@gmail.com>
2013-04-29 12:41:53 -0300

    Currency converter module code

---
+++ project-code/app/com/edulify/modules/currency/Converter.java
@@ -0,0 +1,46 @@
+package com.edulify.modules.currency;
+
+import static play.mvc.Controller.async;
+import static play.mvc.Controller.ok;
+
+import org.codehaus.jackson.JsonNode;
+
+import play.libs.F.Function;
+import play.libs.WS;
+import play.mvc.Result;
+
+public class Converter {
+
+  public enum Source {
+    GET_EXCHANGE_RATES
+  }
+
+  public static Double convert(final Double value, String from, String to) {
+    return convert(value, from, to, Source.GET_EXCHANGE_RATES);
+  }
+
+  public static Double convert(final Double value, String from, String to,  Source source) {
+    if (source.equals(Source.GET_EXCHANGE_RATES)) {
+      return withGetExchangeRates(value, from, to);
+    }
+
+    return null;
+  }
+
+  private static Double withGetExchangeRates(final Double value, String from, String to) {
+    String url = String.format("http://www.getexchangerates.com/api/latest.json");
+    WS.Response wsResponse = WS.url(url).get().get(10000l);
+    JsonNode jsonResponse  = wsResponse.asJson().get(0);
+    JsonNode fromRate = jsonResponse.get(from.toUpperCase());
+    JsonNode toRate   = jsonResponse.get(to.toUpperCase());
+
+    if (fromRate == null || toRate == null) {
+      return null;
+    }
+
+    double exchangeRate = new Double(new String(toRate.getTextValue())) /
+                          new Double(new String(fromRate.getTextValue()));
+
+    return value * exchangeRate;
+  }
+}
---
+++ project-code/project/Build.scala
@@ -0,0 +1,23 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "currency-converter"
+  val appVersion      = "1.1"
+
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+    javaCore,
+    javaJdbc,
+    javaEbean
+  )
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here
+    publishArtifact in(Compile, packageDoc) := false,
+    organization := "com.edulify"
+  )
+
+}

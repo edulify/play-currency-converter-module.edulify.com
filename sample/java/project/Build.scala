49c8d58110e8494e47151fd45bda61a05a130f68
Ranieri Valença <ranierivalenca@gmail.com>
2013-04-30 15:37:12 -0300

    Updating version

--- sample/java/project/Build.scala
+++ sample/java/project/Build.scala
@@ -13,7 +13,7 @@ object ApplicationBuild extends Build {
     javaCore,
     javaJdbc,
     javaEbean,
-    "com.edulify" % "currency-converter_2.10" % "1.1.1"
+    "com.edulify" % "currency-converter_2.10" % "1.1.2"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
